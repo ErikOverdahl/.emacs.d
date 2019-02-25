2502277d75f161b8a13fbbb646a1c97af09ed966
Erik Overdahl <erik.overdahl@gmail.com>
2019-02-25 17:44:03 -0500

    Begin tracking init file changes for easier problem management

---
+++ init.el
@@ -0,0 +1,25 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa2" . "http://www.mirrorservice.org/sites/melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa3" . "http://www.mirrorservice.org/sites/stable.melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org-bullets adaptive-wrap olivetti gruvbox-theme ob-ipython elpy dashboard projectile magit counsel smartparens rainbow-delimiters org-pdfview pdf-tools iedit ace-window try which-key use-package))))
