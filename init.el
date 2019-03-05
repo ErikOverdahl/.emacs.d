c3672a4ab2710654660e57ae0de0d0f77389545b
Erik Overdahl <erik.overdahl@gmail.com>
2019-03-05 16:53:48 -0500

    added Work agenda file; added PlantUML to OB; small organizational changes

--- init.el
+++ init.el
@@ -20,17 +20,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(indent-tabs-mode nil)
  '(org-mode-hook
    (quote
     ((lambda nil
        (org-bullets-mode 1))
      variable-pitch-mode ob-ipython-auto-configure-kernels
      #[0 "\300\301\302\303\304$\207"
-	 [add-hook change-major-mode-hook org-show-block-all append local]
-	 5]
+         [add-hook change-major-mode-hook org-show-block-all append local]
+         5]
      #[0 "\300\301\302\303\304$\207"
-	 [add-hook change-major-mode-hook org-babel-show-result-all append local]
-	 5]
+         [add-hook change-major-mode-hook org-babel-show-result-all append local]
+         5]
      org-babel-result-hide-spec org-babel-hide-all-hashes)))
  '(package-selected-packages
    (quote
@@ -58,6 +59,7 @@
  '(org-priority ((t (:inherit fixed-pitch))))
  '(org-property-value ((t (:inherit fixed-pitch))) t)
  '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch))))
  '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
  '(org-todo ((t (:inherit fixed-pitch))))
  '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
