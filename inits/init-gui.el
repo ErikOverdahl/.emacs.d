2d3deea97782176766722c766ceb40c638347e7a
Erik Overdahl <erik.overdahl@gmail.com>
2019-03-20 14:35:58 -0400

    Still not entirely sure of best organization
    The point of a modular init organization is that it is easy to add and
    remove elements by just commenting out one line.
    ALso not sure how to deal with the order of loading things.
    Probably best to start building and make changes as needed.

--- inits/init-gui.el
+++ inits/init-gui.el
@@ -3,3 +3,14 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+
+(use-package gruvbox-theme
+  :ensure t
+  :config
+  (load-theme 'gruvbox-dark-hard t))
+
+(use-package smart-mode-line
+  :ensure t
+  :config
+  (setq sml/theme 'dark)
+  (sml/setup))
