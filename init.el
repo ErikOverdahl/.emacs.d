8a39a024e31729fee1c80bbf477d8b1e9cb359d0
Erik Overdahl <erik.overdahl@gmail.com>
2019-03-29 21:20:44 -0400

    First attempt at loading
    Cross your fingers.

--- init.el
+++ init.el
@@ -1,5 +1,14 @@
-(setq custom-file "~/.emacs.d/inits/init-custom.el")
-(load 'custom-file)
+(setq inits-dir
+      (expand-file-name "inits" user-emacs-directory))
+(setq packages-dir
+      (expand-file-name "Packages" inits-dir))
+(setq custom-file
+      (expand-file-name "init-custom.el" inits-dir))
+
+(add-to-list 'load-path inits-dir)
+(add-to-list 'load-path packages-dir)
+
+(load custom-file 'no-error)
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -15,54 +24,15 @@
 	(package-refresh-contents)
 	(package-install 'use-package))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(indent-tabs-mode nil)
- '(org-mode-hook
-   (quote
-    ((lambda nil
-       (org-bullets-mode 1))
-     variable-pitch-mode ob-ipython-auto-configure-kernels
-     #[0 "\300\301\302\303\304$\207"
-         [add-hook change-major-mode-hook org-show-block-all append local]
-         5]
-     #[0 "\300\301\302\303\304$\207"
-         [add-hook change-major-mode-hook org-babel-show-result-all append local]
-         5]
-     org-babel-result-hide-spec org-babel-hide-all-hashes)))
- '(package-selected-packages
-   (quote
-    (org-bullets adaptive-wrap olivetti gruvbox-theme ob-ipython elpy dashboard magit counsel smartparens rainbow-delimiters org-pdfview pdf-tools iedit ace-window try which-key use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-block ((t (:inherit fixed-pitch))))
- '(org-document-info ((t (:inherit fixed-pitch :foreground "dark orange"))))
- '(org-document-info-keyword ((t (:inherit fixed-pitch))))
- '(org-document-title ((t (:inherit default :weight bold :height 2.0 :underline t :foreground "#f5f5f5"))))
- '(org-done ((t (:inherit fixed-pitch))))
- '(org-level-1 ((t (:inherit default :weight bold :height 1.5 :foreground "#f5f5f5"))))
- '(org-level-2 ((t (:inherit default :weight bold :height 1.375 :foreground "#f5f5f5"))))
- '(org-level-3 ((t (:inherit default :weight bold :height 1.25 :foreground "#f5f5f5"))))
- '(org-level-4 ((t (:inherit default :weight bold :height 1.125 :foreground "#f5f5f5"))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#f5f5f5"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#f5f5f5"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#f5f5f5"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#f5f5f5"))))
- '(org-link ((t (:foreground "royal blue" :underline t))))
- '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-priority ((t (:inherit fixed-pitch))))
- '(org-property-value ((t (:inherit fixed-pitch))) t)
- '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-table ((t (:inherit fixed-pitch))))
- '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
- '(org-todo ((t (:inherit fixed-pitch))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
+(require 'init-gui)
+(require 'init-global)
+(require 'init-modeline)
+(require 'init-small-packages.el)
+(require 'init-acewindow.el)
+(require 'init-iedit-smartparens.el)
+(require 'init-line-wrapping.el)
+(require 'init-counsel.el)
+(require 'init-magit.el)
+(require 'init-elpy.el)             
+(require 'init-obipython.el)        
+(require 'init-pdf-tools.el)       
