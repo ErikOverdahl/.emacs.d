1477cae85fef9c41833fcb0103765aa0fcf94bee
Erik Overdahl <erik.overdahl@gmail.com>
2019-03-31 10:18:01 -0400

    I think counsel-projectile works now

--- inits/Packages/init-projectile.el
+++ inits/Packages/init-projectile.el
@@ -1,23 +1,22 @@
 (use-package projectile
   :ensure t
-  :defer 1
   :init
   (setq-default
    projectile-cache-file (expand-file-name ".projectile-cache" user-emacs-directory)
    projectile-known-projects-file (expand-file-name
                                    ".projectile-bookmarks" user-emacs-directory))
+  (setq projectile-keymap-prefix (kbd "C-c C-p"))
   :config
 ;;  (projectile-global-mode 1)
   (setq-default
    projectile-completion-system 'ivy
    projectile-enable-caching t
-   projectile-mode-line '(:eval (projectile-project-name))))
+   projectile-mode-line '(:eval (projectile-project-name)))
+  (projectile-mode))
 
 (use-package counsel-projectile
   :ensure t
   :config
-  (counsel-projectile-mode)
-  :bind-keymap
-  ("C-c C-p" . projectile-mode-map))
+  (counsel-projectile-mode))
 
 (provide 'init-projectile)
