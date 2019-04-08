afc04c57a3862853f21633980f534e76aadac991
Erik Overdahl <erik.overdahl@gmail.com>
2019-04-08 15:07:40 -0400

    squash! Merge branch 'emacs-jupyter' into distributed_init
    added function to evaluate jupyter-python blocks without 'yes-no' prompt

--- inits/Packages/init-jupyter.el
+++ inits/Packages/init-jupyter.el
@@ -1,4 +1,8 @@
 (use-package jupyter
   :ensure t)
 
+(defun my-org-confirm-babel-evaluate (lang body)
+  (not (string= lang "jupyter-python")))  ; don't ask for ditaa
+(setq org-confirm-babel-evaluate 'my-org-confirm-babel-evaluate)
+
 (provide 'init-jupyter)
